e25cc81b360b2d42544d13d8a8d0cff17049924c
Xargin <cao1988228@163.com>
2019-11-02 15:44:43 +0800

    finish github search experiment

--- src/github.rs
+++ src/github.rs
@@ -1,25 +1,38 @@
 /// Get github trending list
 /// support configure for interested languages
 use reqwest;
+use serde_json::Value;
 
 // https://developer.github.com/v3/
 // curl -G https://api.github.com/search/repositories --data-urlencode "sort=stars" --data-urlencode "order=desc" --data-urlencode "q=language:java"  --data-urlencode "q=created:>`date -v-7d '+%Y-%m-%d'`"
 pub(crate) async fn crawl() -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}", "crawl github");
-    let client = reqwest::Client::new();
+    let client = reqwest::Client::builder().build()?;
     let params = [
-        ("sort", "stars"),
-        ("order", "desc"),
-        ("q", "language:rust"),
-        ("q", "created>2019-11-01"),
+        ("s", "stars"),
+        ("o", "desc"),
+        // a month ago or a week ago
+        ("q", "created:>2019-05-01 language:Rust"),
     ];
 
-    let body = client.get("http://api.github.com/search/repositories")
+    let body = client
+        .get("http://api.github.com/search/repositories")
         .query(&params)
         .send()
         .await?
         .text()
         .await?;
-    println!("github body: {}", body);
+
+    // println!("total repo count : {}", j["total_count"]);
+
+    let j: Value = serde_json::from_str(body.as_str())?;
+    for item in j["items"].as_array().unwrap() {
+        // todo, beautiful table
+        println!(
+            "item : github.com/{}, {}, ",
+            item["full_name"].as_str().unwrap(),
+            item["stargazers_count"]
+        );
+    }
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use reqwest;
 mod github; // use github is not legal
-mod reddit;
 mod hackernews;
+mod reddit;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = reqwest::get("https://xargin.com")
-        .await?
-        .text()
-        .await?;
-
-    //println!("{}", body);
-
     github::crawl().await?;
     reddit::crawl();
     hackernews::crawl();
