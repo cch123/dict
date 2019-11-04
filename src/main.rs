dec22a8a1eb2a73df629323243d3809a1dd89404
Xargin <cao1988228@163.com>
2019-11-05 00:37:40 +0800

    concurrently crawl all sites

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ mod github; // use github is not legal
 mod hackernews;
 mod reddit;
 
+use futures::future;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO, crawl every site concurrently
-
-    github::crawl().await?;
-    reddit::crawl().await?;
-    hackernews::crawl().await?;
+    // concurrently crawl all sites
+    match future::join3(github::crawl(), reddit::crawl(), hackernews::crawl()).await {
+        (Ok(_), Ok(_), Ok(_)) => println!("crawl success"),
+        (a, b, c) => {
+            println!("github resp : {:?}", a);
+            println!("reddit resp : {:?}", b);
+            println!("hackernews resp : {:?}", c);
+        }
+    }
 
     Ok(())
 }
